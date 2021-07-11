7e7fc834e3cf9ba0a558dfea287d904d72cfa6a3
RishwanthPerumandla <rishwanth100@gmail.com>
2021-07-11 16:09:07 +0530

    Privacy Policy and TC updated

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -4,9 +4,100 @@ import Footer from './Footer.js'
 function PrivacyPolicy() {
   return (
     <>
-      <Header />
-      <div>Hello</div>
-      <Footer />
+      <div className='join-us'>
+        <Header />
+        <div
+          class='container px-2 pl-6 py-0 mx-auto'
+          style={{ paddingTop: '10%' }}
+        >
+          <div class='flex px-8 flex-col text-left w-full mb-12'>
+            <h2
+              class=' text-4xl text-center mb-6 font-medium '
+              style={{ fontWeight: 'bold' }}
+            >
+              {' '}
+              Privacy Policy{' '}
+            </h2>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Log Files
+            </h1>
+            <p class='text-xl mb-3'>
+              www.cockcrow.in follows a standard procedure of using log files.
+              These files log visitors when they visit websites. All hosting
+              companies do this and a part of hosting services’ analytics. The
+              information collected by log files include internet protocol (IP)
+              addresses, browser type, Internet Service Provider (ISP), date and
+              time stamp, referring/exit pages, and possibly the number of
+              clicks. These are not linked to any information that is personally
+              identifiable. The purpose of the information is for analyzing
+              trends, administering the site, tracking users’ movement on the
+              website, and gathering demographic information.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Privacy Policies
+            </h1>
+            <p class='text-xl mb-3'>
+              You may consult this list to find the Privacy Policy for each of
+              the advertising partners of www.cockcrow.in . Third-party ad
+              servers or ad networks uses technologies like cookies, JavaScript,
+              or Web Beacons that are used in their respective advertisements
+              and links that appear on www.rivach.com, which are sent directly
+              to users’ browser. They automatically receive your IP address when
+              this occurs. These technologies are used to measure the
+              effectiveness of their advertising campaigns and/or to personalize
+              the advertising content that you see on websites that you visit.
+              Note that www.rivach.com has no access to or control over these
+              cookies that are used by third-party advertisers.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Third Party Privacy Policies
+            </h1>
+            <p class='text-xl mb-3'>
+              Thus, we are advising you to consult the respective Privacy
+              Policies of these third-party ad servers for more detailed
+              information. It may include their practices and instructions about
+              how to opt-out of certain options. You may find a complete list of
+              these Privacy Policies and their links here: Privacy Policy Links.
+              You can choose to disable cookies through your individual browser
+              options. To know more detailed information about cookie management
+              with specific web browsers, it can be found at the browsers’
+              respective websites.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Children's Information
+            </h1>
+            <p class='text-xl mb-3'>
+              Another part of our priority is adding protection for children
+              while using the internet. We encourage parents and guardians to
+              observe, participate in, and/or monitor and guide their online
+              activity. www.cockcrow.in does not knowingly collect any Personal
+              Identifiable Information from children under the age of 10. If you
+              think that your child provided this kind of information on our
+              website, we strongly encourage you to contact us immediately and
+              we will do our best efforts to promptly remove such information
+              from our records.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Online Privacy Policy Only
+            </h1>
+            <p class='text-xl mb-3'>
+              This privacy policy applies only to our online activities and is
+              valid for visitors to our website with regards to the information
+              that they shared and/or collect in www.cockcrow.in . This policy
+              is not applicable to any information collected offline or via
+              channels other than this website. By using our website, you hereby
+              consent to our Privacy Policy and agree to its Terms and
+              Conditions.
+            </p>
+            <p class='text-xl mb-4'>
+              If you have additional questions or require more information about
+              our Privacy Policy, do not hesitate to contact us through email at
+              cockcrowevents@gmail.com
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   )
 }
--- src/components/TandC.js
+++ src/components/TandC.js
@@ -4,9 +4,176 @@ import Footer from './Footer.js'
 function TandC() {
   return (
     <>
-      <Header />
-      <div>Hello</div>
-      <Footer />
+      <div className='join-us'>
+        <Header />
+        <div className=' container px-2 pl-6 py-0 mx-auto'>
+          <div class='flex px-8 flex-col text-left w-full mb-12'>
+            <h2
+              class=' text-4xl text-center mb-6 font-medium '
+              style={{ fontWeight: 'bold' }}
+            >
+              {' '}
+              Terms and Conditions{' '}
+            </h2>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Introduction
+            </h1>
+            <p class='text-xl mb-3'>
+              These Website Standard Terms and Conditions written on this
+              webpage shall manage your use of this website. These Terms will be
+              applied fully and affect to your use of this Website. By using
+              this Website, you agreed to accept all terms and conditions
+              written in here. You must not use this Website if you disagree
+              with any of these Website Standard Terms and Conditions.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Intellectual Property Rights
+            </h1>
+            <p class='text-xl mb-3'>
+              Other than the content you own, under these Terms, Rivach and/or
+              its licensors own all the intellectual property rights and
+              materials contained in this Website.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Restrictions
+            </h1>
+            <p class='text-xl mb-3'>
+              You are specifically restricted from all of the following : <br />{' '}
+              <span></span>
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              1.Publishing any Website material in any other media
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              2.Selling, sublicensing and/or otherwise commercializing any
+              Website material
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              3.Publicly performing and/or showing any Website material
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              4.Using this Website in any way that is or may be damaging to this
+              Website
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              5.Using this Website in any way that impacts user access to this
+              Website
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              6.Using this Website contrary to applicable laws and regulations,
+              or in any way may cause harm to the Website, or to any person or
+              business entity
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              7.Engaging in any data mining, data harvesting, data extracting or
+              any other similar activity in relation to this Website
+            </p>
+            <p class='text-xl mb-3 ml-6'>
+              {' '}
+              8.Using this Website to engage in any advertising or marketing.
+              Certain areas of this Website are restricted from being access by
+              you and Rivach may further restrict access by you to any areas of
+              this Website, at any time, in absolute discretion. Any user ID and
+              password you may have for this Website are confidential and you
+              must maintain confidentiality as well.
+            </p>
+
+            <p></p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Your Content
+            </h1>
+            <p class='text-xl mb-3'>
+              In these Website Standard Terms and Conditions, “Your Content”
+              shall mean any audio, video text, images or other material you
+              choose to display on this Website. By displaying Your Content, you
+              grant rivach a non-exclusive, worldwide irrevocable, sub
+              licensable license to use, reproduce, adapt, publish, translate
+              and distribute it in any and all media.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              No Warranties
+            </h1>
+            <p class='text-xl mb-3'>
+              This Website is provided “as is,” with all faults, and Rivach
+              express no representations or warranties, of any kind related to
+              this Website or the materials contained on this Website. Also,
+              nothing contained on this Website shall be interpreted as advising
+              you.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Limitation of Liability
+            </h1>
+            <p class='text-xl mb-3'>
+              In no event shall Rivach, nor any of its officers, directors and
+              employees, shall be held liable for anything arising out of or in
+              any way connected with your use of this Website whether such
+              liability is under contract. Rivach, including its officers,
+              directors and employees shall not be held liable for any indirect,
+              consequential or special liability arising out of or in any way
+              related to your use of this Website.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Indemnification
+            </h1>
+            <p class='text-xl mb-3'>
+              You hereby indemnify to the fullest extent Rivach from and against
+              any and/or all liabilities, costs, demands, causes of action,
+              damages and expenses arising in any way related to your breach of
+              any of the provisions of these Terms.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Severability
+            </h1>
+            <p class='text-xl mb-3'>
+              If any provision of these Terms is found to be invalid under any
+              applicable law, such provisions shall be deleted without affecting
+              the remaining provisions herein.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Variation of Terms
+            </h1>
+            <p class='text-xl mb-3'>
+              Rivach is permitted to revise these Terms at any time as it sees
+              fit, and by using this Website you are expected to review these
+              Terms on a regular basis.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Assignment
+            </h1>
+            <p class='text-xl mb-3'>
+              Rivach is allowed to assign, transfer, and subcontract its rights
+              and/or obligations under these Terms without any notification.
+              However, you are not allowed to assign, transfer, or subcontract
+              any of your rights and/or obligations under these Terms.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Entire Agreement
+            </h1>
+            <p class='text-xl mb-3'>
+              These Terms constitute the entire agreement between Rivach and you
+              in relation to your use of this Website, and supersede all prior
+              agreements and understandings.
+            </p>
+            <h1 class='text-2xl mb-2' style={{ fontWeight: 'bold' }}>
+              Governing Law &amp; Jurisdiction
+            </h1>
+            <p class='text-xl mb-3'>
+              Law &amp; JurisdictionThese Terms will be governed by and
+              interpreted in accordance with the laws of the State of
+              Karanataka, and you submit to the non-exclusive jurisdiction of
+              the state and federal courts located in telangana for the
+              resolution of any disputes.
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </>
   )
 }
