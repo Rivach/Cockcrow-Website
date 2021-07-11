009b1a9eff5875ece8491ebedae2c54e868a48d5
seelamvinaykumarreddy <seelam18241a05s8@grietcollege.com>
2021-07-11 11:49:30 +0530

    changes

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const YoutubeEmbed = ({ embedId }) => (
-  <div className='video-responsive flex justify-center '>
-    <iframe
-      width='800'
-      height='600'
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder='0'
-      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-      allowFullScreen
-      title='Embedded youtube'
-    />
-  </div>
+const YoutubeEmbed = ({ embedId }) => ( <
+    div className = 'video-responsive flex justify-center ' >
+    <
+    iframe width = "540"
+    height = "255"
+    src = "https://www.youtube.com/embed/sRy5dJg0k-8"
+    title = "YouTube video player"
+    frameborder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen / > <
+    /div>
 )
 
 YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired,
+    embedId: PropTypes.string.isRequired,
 }
 
 export default YoutubeEmbed
